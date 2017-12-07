7975a4ead4e43db10c51fc7c2bd50adfb1cbc660
kkdshka <kkdshka@gmail.com>
2017-12-07 20:45:55 +0200

    Default score is set.

--- src/reducers/scoreReducer.jsx
+++ src/reducers/scoreReducer.jsx
@@ -1,7 +1,7 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    filteredScoreData: []
+    filteredScoreData: filterScoreData('2', loadData())
 };
 
 export function scoreReducer(state = initialState, action) {
